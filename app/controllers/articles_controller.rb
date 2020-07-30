ca900a4f5e8b65e458452f02ac97ffe3fff70604
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-30 21:38:06 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,11 +22,13 @@ class ArticlesController < ApplicationController
   # add edit and update methods here
   # . Since the form will need to know which record is being edited, this will need to be a dynamic route that accepts an :id as a parameter that the controller can access:
   #  route: get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+# Since @article is not a new instance of Article, the inputs on this form, the text field and text area, will be populated with the corresponding object values.
+
 
   def edit
     @article = Article.find(params[:id])
   end
-  
+
 
 
 # This second route will also need to be dynamic, accepting the same :id as a parameter so that the action will know which record is being altered.
