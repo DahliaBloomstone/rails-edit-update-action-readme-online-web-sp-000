48e2d056d5e301558b54a31a8768b18651b2fbd0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-30 21:33:41 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
     get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   end
 
+# This second route will also need to be dynamic, accepting the same :id as a parameter so that the action will know which record is being altered.
 def update
   patch 'articles/:id', to: 'articles#update'
 end
